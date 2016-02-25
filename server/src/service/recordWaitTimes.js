e071f6813b523ae86eaafad93075ab1e5af2e526
Jonathan Chang <jchang@parasoft.com>
2016-02-25 14:36:55 -0800

    add callback to ride recording

--- server/src/service/recordWaitTimes.js
+++ server/src/service/recordWaitTimes.js
@@ -13,7 +13,6 @@ require('moment-timezone');
 
 exports.start = function(parkAPI, parkId) {
 	console.log('Start logging Disneyland wait times...');
-	getData(parkAPI, parkId);
 	var job = new CronJob('00 */10 * * * *', function() {
 		getData(parkAPI, parkId);
 	}, function() {
@@ -109,6 +108,8 @@ function checkAndRecordWaitTimes(parkAPI, parkId) {
 				id: rideData.id,
 				name: rideData.name,
 				park: parkId
+			}, function() {
+
 			});
 
 			var waitTime = new WaitTime({
